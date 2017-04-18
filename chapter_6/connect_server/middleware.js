4f2a8a865e025e748614fa717006c8f57ae4ff49
zhangchenxu <1161888512@qq.com>
2017-04-18 22:06:45 +0800

    no message

--- chapter_6/connect_server/middleware.js
+++ chapter_6/connect_server/middleware.js
@@ -1,1 +1,15 @@
+var connect = require('connect');
+var app = connect();
+app.listen(3000);
 
+function setup(format) {
+  var regexp = '/:(\w+)/g';
+  return function (req, res, next) {
+    var str = format.replace(regexp, function (match, property) {
+      return req[property]
+    });
+    console.log(str);
+    next();
+  }
+}
+app.use(setup(':method :url'));
